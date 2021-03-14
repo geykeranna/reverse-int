ebc2da198572da75d9fb77f965ebed32c0f7ba31
geykeranna <62444758+geykeranna@users.noreply.github.com>
2021-03-14 17:56:19 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let rev = 0; //результат
+  while (true)
+  {
+     if(n == 0) break;
+     rev = rev * 10 + n % 10;
+     n = n / 10 | 0;
+  }
+     return rev;
 }
